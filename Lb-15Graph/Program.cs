592e9f3d62b600e79d27aaff569dcefbc39905ca
Skiif64 <skiif64@yandex.ru>
2023-01-13 18:33:19 +0400

    Lb15

---
+++ Lb-15Graph/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lb_15Graph
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {            
+            var matrix = new int[][]
+            {
+                 new int[]{0, 1, 4, 0 },
+                 new int[]{1, 0, 1, 2 },
+                 new int[]{1, 4, 0, 1 },
+                 new int[]{0, 1, 2, 0 }
+            };
+            var graph = new Graph(4);
+            graph.Insert(0, 1, 1);
+            graph.Insert(1, 2, 1);
+            graph.Insert(1, 3, 2);
+            graph.Insert(2, 1, 4);
+            using(var sw = new StreamWriter("graph.txt"))
+            {
+                sw.Write(graph.Serialize());
+            }
+            using(var sr = new StreamReader("graph.txt"))
+            {
+                var src = sr.ReadToEnd();
+                graph = Graph.Deserialize(src);
+                Console.WriteLine($"Граф: \n{graph}");                
+            }            
+                        
+            Console.ReadLine();
+        }
+    }
+
+    public class Graph
+    {
+        private readonly int[,] _graph;
+        public int Length => _graph.GetLength(0);
+
+        public Graph(int vertex)
+        {
+            _graph = new int[vertex, vertex];
+        }
+
+        public void Insert(int fromIndex, int toIndex, int weight)
+        {
+            _graph[fromIndex, toIndex] = weight;
+            _graph[toIndex, fromIndex] = weight;
+        }
+
+        public string Serialize()
+        {
+            var n = _graph.GetLength(0);            
+            string output = $"{n};{n}\n";           
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {                    
+                    if (_graph[i, j] > 0)
+                    {
+                        output += $"{i};{j};{_graph[i, j]}\n";                        
+                    }
+                }
+            }            
+            return output;
+        }
+
+        public static Graph Deserialize(string source)
+        {
+            var rows = source.Split('\n');            
+            var dimensions = rows[0].Split(';');
+            var vertex = int.Parse(dimensions[0]);            
+
+            var graph = new Graph(vertex);
+            foreach (var row in rows.Skip(1))
+            {
+                if (string.IsNullOrWhiteSpace(row)) 
+                    break;
+                var cols = row.Split(';');
+                var fromIndex = int.Parse(cols[0]);
+                var toIndex = int.Parse(cols[1]);
+                var weight = int.Parse(cols[2]);
+                graph.Insert(fromIndex, toIndex, weight);
+            }
+            return graph;
+        }
+
+        public override string ToString()
+        {
+            var output = "";
+            for (int i = 0; i < Length; i++)
+            {
+                for (int j = 0; j < Length; j++)
+                {
+                    output += _graph[i, j] + " ";
+                }
+                output += '\n';
+            }
+
+            return output;
+        }
+    }
+}
---
+++ ToSEnd/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Linq;
+
+namespace Lb_1Pyramid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите n:");
+            var n = int.Parse(Console.ReadLine());
+            var array = FillRandom(new int[n], 1, 100);
+            Console.WriteLine("Исходный массив:");
+            Console.WriteLine(string.Join(", ", array));
+            var sortedArray = PyramidSorter.Sort(array);
+            Console.WriteLine("Отсортированный массив:");
+            Console.WriteLine(string.Join(", ", sortedArray));
+            Console.ReadLine();
+        }
+
+        public static int[] FillRandom(int[] array, int from, int to)
+        {
+            var random = new Random();
+            var n = array.Length;
+            for (int i = 0; i < n; i++)
+            {
+                array[i] = random.Next(from, to);
+            }
+            return array;
+        }
+
+        class PyramidSorter
+        {
+            private static int Heapify(int[] arr, int i, int n)
+            {
+                int imax;
+                int buffer;
+                if ((2 * i + 2) < n)
+                {
+                    if (arr[2 * i + 1] > arr[2 * i + 2]) imax = 2 * i + 2;
+                    else imax = 2 * i + 1;
+                }
+                else imax = 2 * i + 1;
+                if (imax >= n) return i;
+                if (arr[i] > arr[imax])
+                {
+                    buffer = arr[i];
+                    arr[i] = arr[imax];
+                    arr[imax] = buffer;
+                    if (imax < n / 2) i = imax;
+                }
+                return i;
+            }
+
+            public static int[] Sort(int[] array)
+            {
+                var arr = array.ToArray();
+
+                var n = arr.Length;
+
+                for (int i = n / 2 - 1; i >= 0; --i)
+                {
+                    var iPrev = i;
+                    i = Heapify(arr, i, n);
+                    if (iPrev != i) ++i;
+                }
+
+
+                int buf;
+                for (int k = n - 1; k > 0; --k)
+                {
+                    buf = arr[0];
+                    arr[0] = arr[k];
+                    arr[k] = buf;
+                    int i = 0;
+                    var iPrev = -1;
+                    while (i != iPrev)
+                    {
+                        iPrev = i;
+                        i = Heapify(arr, i, k);
+                    }
+                }
+
+                return arr;
+            }
+
+        }
+    }
+}
